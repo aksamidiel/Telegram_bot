d0f4ed9bc66c32e6201945bad95f50736592987b
aksamidiel <raven-xl@mail.ru>
2020-04-09 16:04:04 +0300

    new changes

--- Telegram_bot/botik.py
+++ Telegram_bot/botik.py
@@ -1,18 +1,30 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import ReplyKeyboardMarkup
+from Telegram_bot.settings import TG_TOKEN, TG_API_URL
 
 
 def sms(bot, update):
     print('Send message /start')
-    bot.message.reply_text('Hello i"m bot\n') # отправка ответа
+    my_keyboard = ReplyKeyboardMarkup([['/start', 'start'], ['Начать']])   #добавление кнопочки
+    bot.message.reply_text('Hello i"m bot {}\n'.format(bot.message.chat.first_name),
+                           reply_markup=my_keyboard)  # отправка ответа
+    print(bot.message())  # печать сообщения в консоль
+
+
+def par(bot, update):  # ресивер отправленного сообщения
+    print(bot.message.text)
+    bot.message.reply_text(bot.message.text)
+
 
 def main():
-    my_bot = Updater("1211557475:AAF7_m9P0rMA-i--3ltYmVSrLDnl8zaR5eI", "https://telegg.ru/orig/bot", use_context=True)
+    my_bot = Updater(TG_TOKEN, TG_API_URL, use_context=True)
+    my_bot.dispatcher.add_handler(CommandHandler('start', sms))  # обработчик комманды start
 
-    my_bot.dispatcher.add_handler(CommandHandler('start', sms))
-
+    my_bot.dispatcher.add_handler(MessageHandler(Filters.regex('Начать'), sms))
+    my_bot.dispatcher.add_handler(MessageHandler(Filters.text, par))  # обработчик текстового сообщения
     my_bot.start_polling()  # проверка о наличии сообщений с telegram
     my_bot.idle()  # работа пока не остановят
 
 
-#if __name__ == '__main__':
-main()
+if __name__ == '__main__':
+    main()
---
+++ Telegram_bot/settings.py
@@ -0,0 +1,2 @@
+TG_TOKEN = "1211557475:AAF7_m9P0rMA-i--3ltYmVSrLDnl8zaR5eI"
+TG_API_URL = "https://telegg.ru/orig/bot"
